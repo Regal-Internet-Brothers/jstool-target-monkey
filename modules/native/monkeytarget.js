09042d078840894b1300980b396afa0dc59e8d00
Sonickidnextgen <SKIVortex@rocketmail.com>
2015-12-08 07:43:01 -0700

    Changed exception handling in release-mode.

--- modules/native/monkeytarget.js
+++ modules/native/monkeytarget.js
@@ -19,9 +19,11 @@ var __monkey_jstool = true;
 // Global variable(s) (Read only):
 
 // This describes if a compiled Monkey program has been run at least once (Safely).
-var __monkey_jstool_has_run_once = true;
+var __monkey_jstool_has_run_once = false;
 
 // Functions:
+
+// This begins the compiled Monkey program, using the arguments specified (If any).
 function executeMonkey(appArgs)
 {
 	if ((typeof CFG_VIRTUALOS_IMPLEMENTED !== 'undefined') && __os_appargs != null)
@@ -29,27 +31,39 @@ function executeMonkey(appArgs)
 		__os_appargs = appArgs;
 	}
 	
-	try
+	if (typeof CFG_CONFIG !== 'undefined' && CFG_CONFIG === "debug")
 	{
-		bbInit();
-		
-		bbMain();
-	}
-	catch( ex )
-	{
-		if (typeof CFG_CONFIG !== 'undefined' && CFG_CONFIG === "debug")
+		try
+		{
+			__monkey_begin();
+		}
+		catch( ex )
 		{
 			if (ex != null)
 			{
-				alert("EXCEPTION CAUGHT: " + ex);
+				alert("EXCEPTION CAUGHT: " + ex + " (" + ex.fileName + ":" + ex.lineNumber + ")");
+				
 				throw ex;
 			}
+			
+			return;
 		}
-		
-		return;
+	}
+	else
+	{
+		__monkey_begin();
 	}
 	
 	__monkey_jstool_has_run_once = true;
+	
+	return;
+}
+
+// Please call 'executeMonkey' instead of this.
+function __monkey_begin()
+{
+	bbInit();
+	bbMain();
 	
 	return;
 }
